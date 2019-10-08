3e61c7193d566611954280c28400115d7fca8d8b
Endowmissy <adeolaayo31@yahoo.com>
2019-10-08 18:24:32 +0100

    users model added

---
+++ model/users.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+      "name": "Ayomikun Emmanuel",
+      "email": "adeolaayo31@yahoo.com",
+      "id ": 1,
+    },
+    {
+      "name": "Sandra Okafor",
+      "email": "sandraifeoma22@gmail.com",
+      "id ": 2,
+    },
+    { "name": "Desmond Edem",
+      "email": "meetdesmond.edem@gmail.com",
+      "id ": 3,
+    },
+    { "name": "Tunde Ajagbe",
+      "email": "tundeajagbea@gmail.com",
+      "id ": 4,
+    }
+  ]
+
+  export default users;

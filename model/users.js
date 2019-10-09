dcb17d643ca73dbea5a37af81aa56e92cd18da7f
iphyokafor <sandraifeoma22@gmail.com>
2019-10-09 09:16:02 +0100

    usermodel-update

--- model/users.js
+++ model/users.js
@@ -1,22 +1,57 @@
-const users = [
-    {
-      "name": "Ayomikun Emmanuel",
-      "email": "adeolaayo31@yahoo.com",
-      "id ": 1,
+const users = [{
+        "name": "Ayomikun Emmanuel",
+        "email": "adeolaayo31@yahoo.com",
+        "id ": 1,
     },
-    {
-      "name": "Sandra Okafor",
-      "email": "sandraifeoma22@gmail.com",
-      "id ": 2,
-    },
-    { "name": "Desmond Edem",
-      "email": "meetdesmond.edem@gmail.com",
-      "id ": 3,
-    },
-    { "name": "Tunde Ajagbe",
-      "email": "tundeajagbea@gmail.com",
-      "id ": 4,
-    }
-  ]
 
-  export default users;
+    {
+        "name": "Sandra Okafor",
+        "email": "sandraifeoma22@gmail.com",
+        "id ": 2,
+    },
+
+    {
+        "name": "Desmond Edem",
+        "email": "meetdesmond.edem@gmail.com",
+        "id ": 3,
+    },
+
+    {
+        "name": "Tunde Ajagbe",
+        "email": "tundeajagbea@gmail.com",
+        "id ": 4,
+    },
+
+    {
+        "name": "Temiloluwa Abiodun-Ojo",
+        "email": "temiloluwaabiodunojo@gmail.com",
+        "id ": 5,
+    },
+
+    {
+        "name": "Ajagunna SIjuade",
+        "email": "mahyor.sam@gmail.com",
+        "id ": 6,
+    },
+
+    {
+        "name": "onothoja oghenemarho",
+        "email": "marho219@gmail.com",
+        "id ": 7,
+    },
+
+    {
+        "name": "Ropo John Olatujoye",
+        "email": "princeolatjohnjoe@yahoo.com",
+        "id ": 8,
+    },
+
+    {
+        "name": "Onayemi Hajarah",
+        "email": "onayemihajar1439@gmail.com",
+        "id ": 9,
+    }
+
+]
+
+export default users;
